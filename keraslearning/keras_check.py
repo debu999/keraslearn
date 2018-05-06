4519b3e1a0adaeb47d559463de80e3f6a10eacdf
Debabrata Patnaik <debabrata_patnaik@live.com>
2018-05-06 22:19:18 +0800

    Test Tensorflow Part 1

--- keraslearning/keras_check.py
+++ keraslearning/keras_check.py
@@ -3,12 +3,14 @@
 import numpy as np
 from keras import backend as kbe
 import os
+import warnings
+warnings.filterwarnings('ignore', '.*do not.*',)
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1" # LEVEL 0: INFO, LEVEL 1: WARNING
 
 # Test keras - Backend interaction
 data = kbe.variable(np.random.random((4, 2))) # create a 4*2 tensor of random numbers
 
-zero_data = keb.zeros_like(data) # create zeros tensor of same size as data
+zero_data = kbe.zeros_like(data) # create zeros tensor of same size as data
 
-print(keb.eval(zero_data))
+print(kbe.eval(zero_data))
